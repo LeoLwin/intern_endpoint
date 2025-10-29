717aef1d1f803e4cf531bad8c6867eae30609c7f
Kaung Htet Lwin <kaung.htet.lwin@team.studioamk.com>
2025-10-29 16:47:36 +0630

    update with dotenv

--- broker/broker.ts
+++ broker/broker.ts
@@ -4,7 +4,7 @@ let theBroker = new Moleculer.ServiceBroker({
   namespace: "StudentManageMentSystem",
   nodeID: "endpoint",
   transporter: {
-    type: "Redis", 
+    type: "Redis",
     // options: {
     //   host: "redis-17181.c8.us-east-1-3.ec2.redns.redis-cloud.com",
     //   port: 17181,
@@ -12,37 +12,49 @@ let theBroker = new Moleculer.ServiceBroker({
     //   db: 0,
     //   tls: {},
     // },
-    options: {
-  host: "selected-longhorn-77963-gcp-usc1-search.upstash.io",
-  port: 6379,
-  password: "F4H39hOAguWlvxw4MWj6TTBVZlV54wXy",
-  db: 0,
-  tls: {},
-}
+    //     options: {
+    //   host: "selected-longhorn-77963-gcp-usc1-search.upstash.io",
+    //   port: 6379,
+    //   password: "F4H39hOAguWlvxw4MWj6TTBVZlV54wXy",
+    //   db: 0,
+    //   tls: {},
+    // }
 
+    // },
+
+    //     RedistHost=selected-longhorn-77963-gcp-usc1-search.upstash.io
+    // RedisPort=6379
+    // RedisPassword
+    options: {
+      host: process.env.RedistHost,
+      port: process.env.RedisPort,
+      password: process.env.RedisPassword,
+      db: 0,
+      tls: {},
+    },
   },
-//   transporter: {
-//   type: "TCP",ၑ
-//   options: {
-//     // endpoint doesn't need a fixed port; leave blank or set a different port
-//     port: 61300
-//   }
-// },
+  //   transporter: {
+  //   type: "TCP",ၑ
+  //   options: {
+  //     // endpoint doesn't need a fixed port; leave blank or set a different port
+  //     port: 61300
+  //   }
+  // },
   cacher: {
     type: "memory",
   },
   logger: console,
   logLevel: "info",
 
-//   created(broker) {
-//     broker.logger.info("created");
-//   },
-//   started(broker) {
-//     broker.logger.info("started");
-//   },
-//   stopped(broker) {
-//     broker.logger.info("stopped");
-//   },
+  //   created(broker) {
+  //     broker.logger.info("created");
+  //   },
+  //   started(broker) {
+  //     broker.logger.info("started");
+  //   },
+  //   stopped(broker) {
+  //     broker.logger.info("stopped");
+  //   },
 });
 
 module.exports = theBroker;
